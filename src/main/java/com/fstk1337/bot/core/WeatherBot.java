2667f57f3a8e1f76d5ffd083a3f2e53c9c0c869b
fstk1337 <vit.shvaichuk@gmail.com>
2022-08-13 00:45:39 +0300

    test message received

--- src/main/java/com/fstk1337/bot/core/WeatherBot.java
+++ src/main/java/com/fstk1337/bot/core/WeatherBot.java
@@ -1,21 +1,33 @@
 package com.fstk1337.bot.core;
 
 import com.fstk1337.bot.util.PropertiesLoader;
-import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
-import org.telegram.telegrambots.meta.generics.BotOptions;
-import org.telegram.telegrambots.meta.generics.LongPollingBot;
 
-public class WeatherBot implements LongPollingBot {
+import java.util.List;
+
+public class WeatherBot extends TelegramLongPollingBot {
+
     @Override
     public void onUpdateReceived(Update update) {
-
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage sendMessage = new SendMessage();
+            sendMessage.setChatId(update.getMessage().getChatId());
+            sendMessage.setText("test");
+            try {
+                execute(sendMessage);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
-    public BotOptions getOptions() {
-        return new DefaultBotOptions();
+    public void onUpdatesReceived(List<Update> updates) {
+        super.onUpdatesReceived(updates);
     }
 
     @Override
